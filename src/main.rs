1ac8f68ef5e835e4dd3d81b8001af1a77fe04284
Drew Dowling <drewdowling37@gmail.com>
2019-05-16 21:45:26 -0700

    draw green and red blocks on a blank canvas

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,41 @@
+#[macro_use]
+extern crate lazy_static;
 extern crate cursive;
 
 use cursive::traits::*;
-use cursive::Cursive;
-use cursive::views::{SelectView, Dialog, LinearLayout, Button, DummyView, EditView};
+use cursive::{Cursive, Printer, theme};
+use cursive::views::{Canvas, Panel};
+use cursive::theme::{BaseColor, ColorStyle};
+
+lazy_static! {
+    static ref CANVAS_SIZE: (u32, u32) = (30, 15);
+    static ref BLOCK: &'static str = "██";
+    static ref PELLET_COLOR: ColorStyle = theme::ColorStyle::new(
+        theme::Color::Light(BaseColor::Red),
+        theme::PaletteColor::View
+    );
+    static ref SNAKE_COLOR: ColorStyle = theme::ColorStyle::new(
+        theme::Color::Dark(BaseColor::Green),
+        theme::PaletteColor::View
+    );
+}
 
 fn main() {
     let mut siv = Cursive::default();
 
-    let select = SelectView::<String>::new()
-        .on_submit(on_submit)
-        .with_id("select")
-        .fixed_size((10, 5));
-    let buttons = LinearLayout::vertical()
-        .child(Button::new("Add new", add_name))
-        .child(Button::new("Delete", delete_name))
-        .child(DummyView)
-        .child(Button::new("Quit", Cursive::quit));
+    let canvas = Canvas::new(())
+        .with_draw(|_, printer: &Printer| {
+            printer.with_color(*PELLET_COLOR, |printer| {
+                printer.print((10, 4), *BLOCK);
+            });
 
-    siv.add_layer(Dialog::around(
-        LinearLayout::horizontal()
-            .child(select)
-            .child(DummyView)
-            .child(buttons)
-    ));
+            printer.with_color(*SNAKE_COLOR, |printer| {
+                printer.print((0, 1), *BLOCK);
+            });
+        })
+        .with_id("canvas")
+        .fixed_size(*CANVAS_SIZE);
 
+    siv.add_layer(Panel::new(canvas).title("Snake"));
     siv.run();
-}
-
-fn add_name(s: &mut Cursive) {
-    fn ok(s: &mut Cursive, name: &str) {
-        s.call_on_id("select", |view: &mut SelectView<String>| {
-            view.add_item_str(name);
-        });
-        s.pop_layer();
-    }
-
-    s.add_layer(
-        Dialog::around(
-            EditView::new()
-                .with_id("name")
-                .fixed_width(10))
-        .title("Enter a new name")
-        .button("Ok", |s| {
-            let name = s.call_on_id("name" , |view: &mut EditView| {
-                view.get_content()
-            }).unwrap();
-            ok(s, &name);
-        })
-        .button("Cancel", |s| {
-            s.pop_layer();
-        })
-    );
-}
-
-fn delete_name(s: &mut Cursive) {
-    let mut select = s.find_id::<SelectView<String>>("select").unwrap();
-    match select.selected_id() {
-        None => s.add_layer(Dialog::info("No name to remove")),
-        Some(focus) => {
-            select.remove_item(focus);
-        }
-    }
-}
-
-fn on_submit(s: &mut Cursive, name: &String) {
-    s.pop_layer();
-    s.add_layer(
-        Dialog::text(format!("Name: {}\nAwesome: yes", name))
-            .title(format!("{}'s info", name))
-            .button("Quit", Cursive::quit)
-    );
 }
